1d59d6a95a20c79bedaa0ab77586c96a7526a9e6
leonguyendotto <leonguyen.to@MSI>
2022-08-18 13:26:06 -0400

    structure & layout

---
+++ bank_demo_web/src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials,
+};
