f465778e9483e52a522f66495c940c40f55d1597
leonguyendotto <leonguyen.to@MSI>
2022-08-18 14:47:30 -0400

    Hero section

--- bank_demo_web/src/App.jsx
+++ bank_demo_web/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
 
